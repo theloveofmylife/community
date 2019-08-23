1c482b22e036ed9c89dd7302f55f6ee9c75f28de
theloveofmylife <605616892@qq.co>
2019-08-23 21:05:47 +0800

    extract github config from code to properties

--- README.md
+++ README.md
@@ -3,6 +3,10 @@
 ## 资料
 仅为训练Spring Boot使用
 
-https://github.com/theloveofmylife/community
+[bootstrap](https://v3.bootcss.com/components/)
+
+[Github OAuth](https://developer.github.com/apps/building-oauth-apps/creating-an-oauth-app/)
+
+[Github 源代码](https://github.com/theloveofmylife/community)
 
 邮箱：605616892@qq.com
---
+++ src/main/java/life/zihuan/community/controller/AuthorizeController.java
@@ -0,0 +1,36 @@
+package life.zihuan.community.controller;
+
+import life.zihuan.community.dto.AccessTokenDTO;
+import life.zihuan.community.dto.GithubUser;
+import life.zihuan.community.provider.GithubProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class AuthorizeController {
+    @Autowired
+    private GithubProvider githubProvider;
+    @Value("${github.client.id}")
+    private String clientId;
+    @Value("${github.client.secret}")
+    private String clientSecret;
+    @Value("${github.client.uri}")
+    private String clientUri;
+    @GetMapping("/callback")
+    public String callback(@RequestParam(name = "code") String code,@RequestParam(name = "state") String state){
+        AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
+        accessTokenDTO.setCode(code);
+        accessTokenDTO.setClient_id(clientId);
+        accessTokenDTO.setRedirect_uri(clientUri);
+        accessTokenDTO.setState(state);
+        accessTokenDTO.setClient_secret(clientSecret);
+        String accessToken = githubProvider.getAccessToken(accessTokenDTO);
+        GithubUser user = githubProvider.getUser(accessToken);
+        System.out.println(user.getId());
+        System.out.println(user.getName());
+        return "index";
+    }
+}
