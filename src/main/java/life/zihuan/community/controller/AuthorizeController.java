d420cf5c20a4fbbefd5298de48ae7c41f8c4d3ff
theloveofmylife <605616892@qq.com>
2020-04-15 11:47:19 +0800

    加入一个测试的hello.html

--- src/main/java/life/zihuan/community/controller/AuthorizeController.java
+++ src/main/java/life/zihuan/community/controller/AuthorizeController.java
@@ -9,6 +9,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 public class AuthorizeController {
     @Autowired
@@ -20,7 +22,8 @@ public class AuthorizeController {
     @Value("${github.client.uri}")
     private String clientUri;
     @GetMapping("/callback")
-    public String callback(@RequestParam(name = "code") String code,@RequestParam(name = "state") String state){
+    public String callback(@RequestParam(name = "code") String code, @RequestParam(name = "state") String state,
+                           HttpServletRequest request){
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setCode(code);
         accessTokenDTO.setClient_id(clientId);
@@ -31,6 +34,13 @@ public class AuthorizeController {
         GithubUser user = githubProvider.getUser(accessToken);
         System.out.println(user.getId());
         System.out.println(user.getName());
-        return "index";
+        if (user !=null){
+            //登录成功,写cookie和session
+            request.getSession().setAttribute("user",user);
+            return "redirect:/";
+        }else {
+            //登陆失败
+            return "redirect:/";
+        }
     }
 }
